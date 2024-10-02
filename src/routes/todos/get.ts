77374845715706331f279993211b75552d9dea45
Gaizka Allende <gaizka@allende.org>
2024-10-02 09:44:27 +0100

    add missing template tag

--- src/routes/todos/get.ts
+++ src/routes/todos/get.ts
@@ -2,7 +2,7 @@ import { Context } from 'hono'
 import { Low } from 'lowdb'
 
 import { Database } from '../../types'
-import { renderHTMLDocument } from '../../components/document'
+import screen from '../../components/document'
 import { renderTodosContainer } from '../../components/todo'
 
 export const response = (id: string) => `PUT /todo/${id}`
@@ -10,7 +10,5 @@ export const response = (id: string) => `PUT /todo/${id}`
 export default async (c: Context) => {
   const db = c.get('db') as Low<Database>
   const username = c.get('username') as string
-  return c.html(
-    renderHTMLDocument(renderTodosContainer(db.data.todos[username])),
-  )
+  return c.html(screen`${renderTodosContainer(db.data.todos[username])}`)
 }
